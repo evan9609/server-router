cdcf1cfd0c51e77755321eaa6b1f56f7702ff83b
arisevan <115055847+evan9609@users.noreply.github.com>
2023-12-05 11:54:43 +0800

    課程api

--- routes/course-route.js
+++ routes/course-route.js
@@ -1,3 +1,5 @@
+const { course } = require('.');
+
 const router = require('express').Router();
 const Course = require('../models').course;
 const courseValidation = require('../validation').courseValidation;
@@ -7,6 +9,29 @@ router.use((req,res,next)=>{
   next()
 })
 
+// 獲得系統中的所有課程
+router.get('/',async(req,res)=>{
+  try{
+    let courseFound = await Course.find().populate('instructor',['username','email']).exec();
+    return res.send(courseFound)
+  }catch(e){
+    return res.status(500).send(e)
+  }
+})
+
+//用課程id尋找課程
+router.get('/:_id',async(req,res)=>{
+  let { _id }= req.params;
+  try{
+    let courseFound = await Course.findOne({ _id }).populate('instructor',['email']).exec()
+    return res.send(courseFound)
+  }catch(e){
+    return res.status(500).send(e)
+  }
+
+})
+
+// 新增課程
 router.post('/',async (req,res)=>{
   // 驗證數據符合規範
   let {error} =  courseValidation(req.body);
@@ -28,7 +53,65 @@ router.post('/',async (req,res)=>{
   }catch(e){
     return res.status(500).send('創建失敗')
   }
-  
-
 })
+
+// 更改課程
+router.patch('/:_id',async(req,res)=>{
+  let { error } = courseValidation(req.body);
+  if(error) return res.status(400).send(error.details[0].message)
+
+  let { _id } = req.params;
+
+  try{
+    let courseFound = await Course.findOne({_id});
+    console.log(courseFound)
+    if(!courseFound){
+      return res.status(400).send('找不到相關課程,無法更新課程內容')
+    }
+    // 使用者必須是此課程講師才能編輯課程
+    console.log('確認講師身分...')
+    if(courseFound.instructor.equals(req.user._id)){
+      let updatedCourse = await Course.findOneAndUpdate({_id},req.body,{
+        new: true,
+        runValidators: true
+      })
+      return res.send({
+        msg: '課程更新成功',
+        updatedCourse
+      })
+    }else{
+      return res.status(403).send('只有此課程講師才能編輯課程')
+    }
+  }catch(e){
+    return res.status(500).send(e)
+  }
+})
+
+// 刪除課程
+router.delete('/:_id', async(req,res)=>{
+  let { error } = courseValidation(req.body);
+  if(error) return res.status(400).send(error.details[0].message)
+
+  let { _id } = req.params;
+
+  try{
+    let courseFound = await Course.findOne({_id});
+    console.log(courseFound)
+    if(!courseFound){
+      return res.status(400).send('找不到相關課程,無法刪除')
+    }
+    // 使用者必須是此課程講師才能刪除課程
+    console.log('確認講師身分...')
+    if(courseFound.instructor.equals(req.user._id)){
+      await Course.deleteOne({ _id }).exec()
+      return res.send('課程已被刪除')
+      
+    }else{
+      return res.status(403).send('只有此課程講師才能刪除課程')
+    }
+  }catch(e){
+    return res.status(500).send(e)
+  }
+})
+
 module.exports = router;
