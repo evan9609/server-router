ba1a30a6e3a6045db7b7817930b3e1294bceef3d
arisevan <115055847+evan9609@users.noreply.github.com>
2023-12-22 00:38:47 +0800

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const passport = require('passport')
 // require的是function,所以可以直接執行
 require('./config/passport')(passport)
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
 
 //
 mongoose.connect('mongodb://127.0.0.1/mernDB').then(()=>{
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 //在route之前使用
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'client', 'build')))
+// app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.use('/api/user', authRoute)
 // course route只有講師可以使用,應該被JWT保護
@@ -31,12 +31,12 @@ app.use('/api/user', authRoute)
 app.use('/api/courses',passport.authenticate('jwt',{session: false}), courseRoute)
 
 // 只有登入系統的人才能夠去新增或是註冊課程
-if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
-  app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+// if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 
-  })
-}
+//   })
+// }
 
 app.listen(8080,()=>{
   console.log('後端伺服器運行在port 8080')
